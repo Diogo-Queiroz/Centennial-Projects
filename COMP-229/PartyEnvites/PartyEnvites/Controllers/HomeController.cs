a47242167367d4f54f5a92e3723c0d3b16280cfb
Diogo Queiroz <diogoqueirooz1212@gmail.com>
2019-09-12 16:20:34 -0400

    lecture about render pages, lists

---
+++ COMP-229/PartyEnvites/PartyEnvites/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PartyEnvites.Models;
+
+namespace PartyEnvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            ViewBag.Greeting = "Humans";
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestReponse guestResponse)
+        {
+            Repository.AddReponse(guestResponse);
+            return View("Thanks", guestResponse);
+        }
+
+        public ViewResult ListResponse()
+        {
+            return View(Repository.Reponses.Where(r => r.WillAttend == true));
+        }
+    }
+}
---
+++ COMP-229/PartyEnvites/PartyEnvites/Models/Repository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyEnvites.Models
+{
+    public class Repository
+    {
+        private static List<GuestReponse> responses = new List<GuestReponse>();
+
+        public static IEnumerable<GuestReponse> Reponses
+        {
+            get
+            {
+                return responses;
+            }
+        }
+
+        public static void AddReponse(GuestReponse response)
+        {
+            responses.Add(response);
+        }
+    }
+}
