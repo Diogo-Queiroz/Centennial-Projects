5077e6d78b82d925170a6bf37d7e38389a67d637
Diogo Queiroz <diogoqueirtooz1212@gmail.com>
2019-09-15 14:52:17 -0400

    exercise 2 ready

---
+++ COMP-228/ASSIGMENTS/LAB1/src/exercise2/BankAccount.java
@@ -0,0 +1,65 @@
+package exercise2;
+
+import javax.swing.*;
+
+public class BankAccount {
+    private int AccountNumber;
+    private String Name;
+    private double Balance;
+
+    BankAccount (int accNum, String name, double bal) {
+        AccountNumber = accNum;
+        Name = name;
+        Balance = bal;
+    }
+
+    BankAccount () {
+        AccountNumber = 0;
+        Name = "";
+        Balance = 0;
+    }
+
+    public void withdran(double Amount) {
+        if (Balance - Amount < 0) {
+            JOptionPane.showMessageDialog(null, "I'm sorry, but you don't have enough money to process this request");
+        } else if (Balance > 0) {
+            Balance = Balance - Amount;
+        }
+        else {
+            JOptionPane.showMessageDialog(null, "First make a deposit, thank you");
+        }
+    }
+
+    public void deposit(double Amount) {
+        Balance = Balance + Amount;
+    }
+
+    public void getAccountInfo() {
+        JOptionPane.showMessageDialog(null,
+                String.format(
+                        "Account Number [%d], Owner's Name [%s], Account Balance [%.2f]",
+                        getAccountNumber(),
+                        getName(),
+                        getBalance()));
+    }
+
+    // Getter and Setter
+    public int getAccountNumber() {
+        return AccountNumber;
+    }
+    public String getName() {
+        return Name;
+    }
+    public double getBalance() {
+        return Balance;
+    }
+    public void setAccountNumber(int accountNumber) {
+        AccountNumber = accountNumber;
+    }
+    public void setName(String name) {
+        Name = name;
+    }
+    public void setBalance(double balance) {
+        Balance = balance;
+    }
+}
---
+++ COMP-228/ASSIGMENTS/LAB1/src/exercise2/BankAccountTest.java
@@ -0,0 +1,29 @@
+package exercise2;
+
+import javax.naming.NameAlreadyBoundException;
+import javax.swing.*;
+
+public class BankAccountTest {
+    public static void main(String[] args) {
+        int accountId;
+        String name;
+        double balance;
+        double amount;
+
+        name = JOptionPane.showInputDialog("What's your name?");
+        accountId = Integer.parseInt(JOptionPane.showInputDialog("Your Account Number"));
+        balance = Double.parseDouble(JOptionPane.showInputDialog("Initial Account Balance"));
+
+        BankAccount bankAccount = new BankAccount(accountId, name, balance);
+
+        bankAccount.getAccountInfo();
+        amount = Double.parseDouble(JOptionPane.showInputDialog("Make a Deposit of"));
+        bankAccount.deposit(amount);
+
+        bankAccount.getAccountInfo();
+        amount = Double.parseDouble(JOptionPane.showInputDialog("How much do you want to withdraw?"));
+        bankAccount.withdran(amount);
+
+        bankAccount.getAccountInfo();
+    }
+}
