c27e35f23719973d4d29d459c9df86c76d867c6a
Diogo Queiroz <diogoqueirtooz1212@gmail.com>
2019-10-30 13:57:40 -0400

    Update the Unity Project

---
+++ COMP-391/LEC/SPACE2D-FALL2019/Assets/Scripts/Mover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Mover : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+    public float speed = 20.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        if (_rigidbody2D.tag.Equals("Laser"))
+            _rigidbody2D.velocity = Vector2.right * speed;
+        else if (_rigidbody2D.tag.Equals("Asteroid"))
+            _rigidbody2D.velocity = Vector2.left * speed;
+    }
+}
--- COMP-391/LEC/SPACE2D-FALL2019/Assets/Scripts/PlayerController.cs
+++ COMP-391/LEC/SPACE2D-FALL2019/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,38 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D player;
+    private Rigidbody2D _player;
 
-    private float speed = 30.0f;
+    public float speed = 30.0f;
+    public GameObject laser;
+    public Transform laserSpawn;
+    public float fireRate = 0.5f;
+
+    public Vector2 bottomLeftBoundarie;
+    public Vector2 topRigthBoundarie;
+    
+    private float counter = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _player = GetComponent<Rigidbody2D>();
+        _player.transform.position = new Vector2(-6.5f, 0.0f);
+    }
+
+    void Update()
+    {
+        counter += Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Space) && counter > fireRate)
+        {
+            Instantiate(laser, laserSpawn.position, laser.transform.rotation);
+            counter = 0.0f;
+        }
     }
 
     // Update is called once per frame
@@ -19,6 +41,9 @@ public class PlayerController : MonoBehaviour
         var moveVertical = Input.GetAxis("Vertical") * speed;
         var moveHorizontal = Input.GetAxis("Horizontal") * speed;
         
-        player.velocity = new Vector2(moveHorizontal, moveVertical);
+        _player.velocity = new Vector2(moveHorizontal, moveVertical);
+        _player.position = new Vector2(
+            Mathf.Clamp(_player.position.x, bottomLeftBoundarie.x, topRigthBoundarie.x),
+            Mathf.Clamp(_player.position.y, bottomLeftBoundarie.y, topRigthBoundarie.y));
     }
 }
